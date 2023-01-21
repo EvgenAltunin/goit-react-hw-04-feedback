c2c94f6fcc56015888ba11ffdffd61e8ad8b4a7b
Yevhen2701 <106544836+EvgenAltunin@users.noreply.github.com>
2023-01-21 18:25:47 +0200

    Class was replased to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,56 @@
-import { Feedback } from "components/Feedback/Feedback";
+import { useState } from 'react';
+import { Section } from 'components/Section/Section';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Statistics } from 'components/Statistics/Statistics';
+import { Notification } from 'components/Notification/Notification';
 
+export function App() {
 
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-export const App = () => {
-  return (
-    <div>
-        <Feedback></Feedback>
-    </div>
-  );
-};
+  const handleLeaveFeedback = event => {
+    switch (event.target.name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  const totalFeedback = good + neutral + bad;
+  const positiveFeedbackPercentage = Math.round((good * 100) / totalFeedback)
+
+return (
+  <>
+    <Section title="Please leave feedback">
+      <FeedbackOptions
+        options={[ 'good', 'neutral', 'bad' ]}
+        onLeaveFeedback={handleLeaveFeedback}
+      />
+    </Section>
+    <Section title="Statistics" children>
+      {totalFeedback > 0 ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={totalFeedback}
+          positivePercentage={positiveFeedbackPercentage}
+        />
+      ) : (
+        <Notification message="No feedback given" />
+      )}
+    </Section>
+  </>
+);
+}
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,22 +1,30 @@
 import PropTypes from 'prop-types'; 
 import { Container, Control } from "components/FeedbackOptions/FeedbackOptions.styled";
 
-export const FeedbackOptions = ({options, onLeaveFeedback}) => {
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+// отримання масива значень для кнопок, кожне значення з великої букви
+    const capitalizedOptions = [];
+    for (const buttonOption of options) {
+      capitalizedOptions.push(buttonOption[0].toUpperCase() + buttonOption.slice(1)
+      );
+    }
+//======================================================================
     return (
-        <Container>
-            {options.map(option => {
-                return (
-                    <Control
-                        type="button"
-                        name={option}
-                        key={option}
-                        onClick={onLeaveFeedback}
-                    >{option}
-                    </Control>)
-            })}
-            
-        </Container>
-    )
+      <Container>
+        {capitalizedOptions.map(capitalizedOption => {
+          return (
+            <Control
+              type="button"
+              key={capitalizedOption.toLowerCase()}
+              name={capitalizedOption.toLowerCase()}
+              onClick={onLeaveFeedback}
+            >
+              {capitalizedOption}
+            </Control>
+          );
+        })}
+      </Container>
+    );
 }
 
 FeedbackOptions.propTypes = {
